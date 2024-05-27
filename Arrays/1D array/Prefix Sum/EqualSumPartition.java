a58d6411bbe304a127568d5f3f417b84ca2588e7
Predator-315 <soumajitdasr3244@outllook.com>
2024-05-27 20:52:41 +0530

    Update Questions on Array

--- Arrays/1D array/Prefix Sum/EqualSumPartition.java
+++ Arrays/1D array/Prefix Sum/EqualSumPartition.java
@@ -1,3 +1,10 @@
+//Check if we can partition the array into two subarrays with equal sum. More,fomrally, check that the prefix sum of a part of array is equal to the suffix sum of the rest of the array.
+//Algo:
+//1. Calculate the total sum of the array
+//2. For each element a[i], calculate its prefix sum & suffix sum of the next element a[i+1] (total sum-suffix sum)
+//if prefix sum of a[i] == suffix sum of a[i+1] then return true
+//if whole array is traversed without statisfying the condition then return false
+
 class EqualSumPartition 
 {
     static boolean checkEqualSumPartition(int a[])
